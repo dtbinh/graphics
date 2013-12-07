04416749cdebdb47ac8e42552c90bd790de892e4
ahusain <ahusain@nrec-014710.(none)>
2013-12-07 15:31:53 -0500

    initial commit: 4 semesters of assignments

---
+++ Fall2013/p2/src/p2/project.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file project.cpp
+ * @brief Geometry project
+ *
+ * @author H. Q. Bovik (hqbovik)
+ * @bug Unimplemented
+ */
+
+#include "project.hpp"
+#include "application/opengl.hpp"
+
+/*
+   A namespace declaration. All project files use this namespace.
+   Add this declaration (and its closing) to all source/headers you create.
+   Note that all #includes should be BEFORE the namespace declaration.
+ */
+namespace _462 {
+
+// definitions of functions for the GeometryProject class
+
+// constructor, invoked when object is allocated
+GeometryProject::GeometryProject() { }
+
+// destructor, invoked when object is de-allocated
+GeometryProject::~GeometryProject() { }
+
+/**
+ * Initialize the project, doing any necessary opengl initialization.
+ * @param camera An already-initialized camera.
+ * @param mesh The mesh to be rendered and subdivided.
+ * @param texture_filename The filename of the texture to use with the mesh.
+ *  Is null if there is no texture data with the mesh or no texture filename
+ *  was passed in the arguments, in which case textures should not be used.
+ * @return true on success, false on error.
+ */
+bool GeometryProject::initialize( const Camera* camera, const MeshData* mesh, const char* texture_filename )
+{
+    this->mesh = *mesh;
+
+    // TODO opengl initialization code
+    return true;
+}
+
+/**
+ * Clean up the project. Free any memory, etc.
+ */
+void GeometryProject::destroy()
+{
+  // TODO any cleanup code
+}
+
+/**
+ * Clear the screen, then render the mesh using the given camera.
+ * @param camera The logical camera to use.
+ * @see scene/camera.hpp
+ */
+void GeometryProject::render( const Camera* camera )
+{
+  // TODO render code
+}
+
+
+/**
+ * Subdivide the mesh that we are rendering using Loop subdivison.
+ */
+void GeometryProject::subdivide()
+{
+    // TODO perform a single subdivision.
+}
+
+
+
+} /* _462 */
+
